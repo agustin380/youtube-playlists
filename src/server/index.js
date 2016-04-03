2f991b4360514b161145c9431d235573c57ade77
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2016-04-02 22:17:04 -0300

    Move hello world example to HTML file

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,24 @@
-var http = require('http');
+const http = require('http');
+const fs = require('fs');
 
-http.createServer(function(request, response) {
-  request.on('error', function(err) {
+http.createServer((request, response) => {
+  request.on('error', (err) => {
     console.error(err);
     response.statusCode = 400;
     response.end();
   });
-  response.on('error', function(err) {
+  response.on('error', (err) => {
     console.error(err);
   });
   if (request.method === 'GET' && request.url === '/echo') {
-    response.end('<html><body><h1>Hello, World!</h1></body></html>');
+    fs.readFile('./src/server/views/index.html', 'utf8', (err, data) => {
+      if (err) {
+        return console.log(err);
+      }
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(data);
+      response.end();
+    });
   } else {
     response.statusCode = 404;
     response.end();
