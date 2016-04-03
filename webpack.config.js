31689ba406520e2d3e149fbedce349499f54470c
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2016-04-03 16:53:45 -0300

    Add Youtube Player API

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react'],
