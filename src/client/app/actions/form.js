7b37a8ac535b416de8bb33fdf51f5c9949f0cdb7
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2016-07-18 21:58:53 -0300

    Minor refactoring in form actions

--- src/client/app/actions/form.js
+++ src/client/app/actions/form.js
@@ -3,12 +3,14 @@ import moment from 'moment';
 import { addPlayListItem } from './playlist.js';
 
 export const submitPlaylistItem = (videoId) => (dispatch) => {
-  window.gapi.client.load('youtube', 'v3').then(() => Promise.all([
-    window.gapi.client.youtube.videos.list({
+  const { client } = window.gapi;
+
+  client.load('youtube', 'v3').then(() => Promise.all([
+    client.youtube.videos.list({
       part: 'snippet',
       id: videoId,
     }),
-    window.gapi.client.youtube.videos.list({
+    client.youtube.videos.list({
       part: 'contentDetails',
       id: videoId,
     }),
@@ -17,5 +19,6 @@ export const submitPlaylistItem = (videoId) => (dispatch) => {
     const duration = moment.duration(contentDetails.result.items[0].contentDetails.duration);
     dispatch(addPlayListItem(title, videoId, duration));
   }));
+
   return Promise.resolve();
 };
